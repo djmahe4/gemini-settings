464b261e282f56d3e13ddf6f7d4c305c4eb7039d
Maheshwar Anup <137691824+djmahe4@users.noreply.github.com>
2025-12-24 15:50:07 +0530

    Update yolo_config.bat

--- yolo_config.bat
+++ yolo_config.bat
@@ -1,4 +1,4 @@
 venv/Scripts/actvate
-pip install torch==2.3.1+cu121 torchvision==0.18.1+cu121 torchaudio==2.3.1+cu121 --index-url https://download.pytorch.org/whl/cu121
+pip install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cu128
 pip install ultralytics --no-deps
 python -c "import torch; print(torch.cuda.is_available())"
